84c65ad59937c4a1d8e9e869be80a09ae25644f0
Tina Zhang <tina.zhang@chinanetcloud.com>
2016-07-15 03:34:01 +0000

    defined graphs list

--- nc_auto_screens.py
+++ nc_auto_screens.py
@@ -45,7 +45,18 @@ def graphsForScreens(hosts):
     if not hosts:
         print "hosts list is empty, exit..."
         sys.exit(1)
-    graphs_stand_list = [ 'CPU %', 'Memory Usage','IOStat', 'Ethx Network Traffic', 'nginx', 'MySQL Connections', 'Tomcat*Sessions','Vmstat Swap In-Out']
+    graphs_stand_list = [ 'CPU %', 'Memory Usage','IOStat', 'Vmstat Swap In-Out', 
+                        'Ethx Network Traffic',
+                        'Nginx', 'Apache', 'PHP-FPM Processes',
+                        'MySQL*Connections', 'MySQL*Queries', 'MySQL*InnoDB',
+                        'Tomcat*Heap Memory usage', 'Tomcat*Worker Threads', 'Tomcat*Sessions',
+                        'Java*Heap Memory usage', 'Java*Worker Threads', 'Java*Sessions',
+                        'HaProxy back/front session',
+                        'Redis*Connections',
+                        'Memcache*Connections',
+                        'MongoDB*Queries-Requests', 'MongoDB*Connections', 'MongoDB*Performance'
+                        'Postgres*Connections', 'Postgres*Cache Hit/Read'
+                        ]
     graphs_dict = {}
     for hostid in hosts:
         host_graphs_list = []
@@ -65,6 +76,7 @@ def graphsForScreens(hosts):
             continue
         graphs_dict[hostid] = host_graphs_list
     return graphs_dict
+
 def CreateScreen(screen_name, HostList):
     screen_name='~Customer - ' + screen_name
     # Check if Screen exist
@@ -72,6 +84,7 @@ def CreateScreen(screen_name, HostList):
     if result:
         print "\nScreen %s already exists." % screen_name
         sys.exit(1)
+
     screen_vsize = len(HostList)
     num = []
     for i in HostList:
@@ -79,6 +92,7 @@ def CreateScreen(screen_name, HostList):
     screen_hsize = max(num)
     zapi.screen.create({"name": screen_name, "hsize": screen_hsize, "vsize": screen_vsize})
     screenid = zapi.screen.get({ "output": "screenid", "filter": { "name": screen_name} })[0]['screenid']
+
     y = 0
     for h in HostList:
         x = 0
@@ -95,7 +109,7 @@ def CreateScreen(screen_name, HostList):
             x += 1
         y += 1
 
-    print "Successfully create Screen %s!" % screen_name
+    print "Successfully Create Screen: %s!" % screen_name
 
 if __name__=='__main__':
     Login()
